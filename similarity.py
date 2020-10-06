85f1bc55ddbae6afff711f0436fa05b3766c2fb3
soham das <dassoham0801@gmail.com>
2020-10-07 01:42:33 +0530

    Added similarity checking script

---
+++ similarity.py
@@ -0,0 +1,16 @@
+from skimage import io
+import cv2
+import numpy as np
+
+
+def dominant(path):
+    img = io.imread(path)
+    pixels = np.float32(img.reshape(-1, 3))
+
+    n_colors = 5
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 200, .1)
+    flags = cv2.KMEANS_RANDOM_CENTERS
+
+    _, labels, palette = cv2.kmeans(pixels, n_colors, None, criteria, 10, flags)
+    _, counts = np.unique(labels, return_counts=True)
+    return palette[np.argmax(counts)]
