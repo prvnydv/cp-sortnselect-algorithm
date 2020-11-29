84ab105cb88f1da0e7f973854e754f097e37d9d4
pical-tech <py@pical.in>
2020-11-29 11:31:16 +0530

    rewritten to change the params and files manipulation with s3

--- image_frequency.py
+++ image_frequency.py
@@ -7,7 +7,7 @@ from utils import read_pillow_image_from_s3
 from utils import write_cv2_image_to_s3, read_with_cv2_from_generated_temp_file
 
 model_face_extraction = cv2.dnn.readNetFromCaffe("face_extraction_model/deploy.prototxt", 'face_extraction_model/weights.caffemodel')
-def img_frequency(s3_uri,job_uid,a,j):
+def img_frequency(s3_uri,job_uid,face_image_mapper):
 
     image = read_with_cv2_from_generated_temp_file(s3_uri)
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -18,6 +18,7 @@ def img_frequency(s3_uri,job_uid,a,j):
 
     model_face_extraction.setInput(blob)
     detections = model_face_extraction.forward()
+    
     for i in range(0, detections.shape[2]):
         box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
         (startX, startY, endX, endY) = box.astype("int")
@@ -32,26 +33,25 @@ def img_frequency(s3_uri,job_uid,a,j):
             frame = image[startY:endY, startX:endX]
             if len(frame)>200 and len(frame[0])>200 and mean>0:
 
-                write_cv2_image_to_s3(frame, 'faces_extracted', f'{i}${s3_uri.split("/")[-1]}', job_uid)
+                #write_cv2_image_to_s3(frame, folder, f'{i}${s3_uri.split("/")[-1]}', job_uid)
                 try:
                     # Saving New Faces and appending in image maps when similar image is found
-                    other_img=face_recognition.load_image_file(read_pillow_image_from_s3(f"s3://pical-backend-dev/faces_extracted/{job_uid}/{i}${s3_uri.split("/")[-1]}"))
-                    other_enc=face_recognition.face_encodings(other_img)[0]
-                    a_list=list(a.keys())
+                    #other_img=face_recognition.load_image_file(read_pillow_image_from_s3(f"s3://pical-backend-dev/faces_extracted/{job_uid}/{i}${s3_uri.split("/")[-1]}"))
+                    face_enc_of_image=face_recognition.face_encodings(frame)[0]
+                    face_image_list = list(face_image_mapper.keys())
                     flag=0
-                    for k in range(len(a_list)):
-                        if face_recognition.compare_faces([a[a_list[k]]['face_vector']], other_enc)[0]:
+                    for k in range(len(face_image_list)):
+                        if face_recognition.compare_faces([face_image_mapper[face_image_list[k]]['face_vector']], other_enc)[0]:
                             # Append in Image map if faces are similar
-                            [a[a_list[k]]['images']][0].append(s3_uri)
+                            face_image_mapper[face_image_list[k]]['images'].append(f"{s3_uri.split("/")[-1]}")
                             flag=1
                             break
                         else:
                             continue
                     if flag==0:
                         # Save new face
-                        a[f"{i}${s3_uri.split("/")[-1]}"]={'face_vector':other_enc,'images':[{s3_uri}]}
-
-                                                
+                        face_image_mapper[f"{i}${s3_uri.split("/")[-1]}"]={'face_vector':other_enc,'images':[f"{s3_uri.split("/")[-1]}"]}                               
                     
                 except:
                     pass
+    return face_image_mapper
