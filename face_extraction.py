809369507f920f49ca42e5f38e11d222b85aff09
pical-tech <py@pical.in>
2021-01-25 02:09:00 +0530

    eliminating condition of face to be atleast 200x200

--- face_extraction.py
+++ face_extraction.py
@@ -3,7 +3,7 @@ from cv2 import cv2
 import os
 import numpy as np
 from utils import read_pillow_image_from_s3
-from utils import read_with_cv2_from_generated_temp_file, write_cv2_image_to_s3
+from utils import read_with_cv2_from_generated_temp_file_gdrive, write_cv2_image_to_s3
 
 def detect_blur_fft(image, size=60, thresh=0, vis=True):
     (h, w) = image.shape
@@ -18,9 +18,9 @@ def detect_blur_fft(image, size=60, thresh=0, vis=True):
     return mean
 
 model_face_extraction = cv2.dnn.readNetFromCaffe("face_extraction_model/deploy.prototxt", 'face_extraction_model/weights.caffemodel')
-def img_to_faces(job_uid, s3_uri, folder):
+def img_to_faces(job_uid, url, drive, folder):
 
-  image = read_with_cv2_from_generated_temp_file(s3_uri)
+  image = read_with_cv2_from_generated_temp_file_gdrive(drive, url)
   gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
   mean = detect_blur_fft(gray, size=60)
   print(f"Read the image mean :: {mean}")
@@ -37,10 +37,6 @@ def img_to_faces(job_uid, s3_uri, folder):
       (startX, startY, endX, endY) = box.astype("int")
 
       confidence = detections[0, 0, i, 2]
-      count=0
-
-    # If confidence > 0.5, save it as a separate file
-      if (confidence > 0.5):
-          count += 1
+      if (confidence > 0.25):
           frame = image[startY:endY, startX:endX]
-          if len(frame)>200 and len(frame[0])>200 and mean>0: write_cv2_image_to_s3(frame, folder, f"{i}${s3_uri.split('/')[-1]}", job_uid)
+          write_cv2_image_to_s3(frame, folder, f"{i}${url}", job_uid)
